2726af961b1ddc7f1198e0bd8504d5363d46b41c
Marcin Zieliński <marcin.mariusz.zielinski@gmail.com>
2024-03-14 19:17:21 +0100

    Set form & assert

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -22,5 +23,24 @@ public class SignUpTest {
         myAccBttn.click();
         WebElement sngUpBttn = driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1);
         sngUpBttn.click();
+
+        //set form & assert
+        WebElement firstName = driver.findElement(By.name("firstname"));
+        firstName.sendKeys("Marcin");
+        WebElement lastName = driver.findElement(By.name("lastname"));
+        lastName.sendKeys("Testerski");
+        WebElement phoneNumber = driver.findElement(By.name("phone"));
+        phoneNumber.sendKeys("123456789");
+        WebElement emailAddress = driver.findElement(By.name("email"));
+        emailAddress.sendKeys("test2@tester.com");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("test123");
+        WebElement confirmPassword = driver.findElement(By.name("confirmpassword"));
+        confirmPassword.sendKeys("test123");
+        WebElement signUpBttn = driver.findElement(By.xpath("//button[contains(@class, 'signupbtn')]"));
+        signUpBttn.click();
+
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+        Assert.assertTrue(heading.getText().contains("Hi, Marcin Testerski"));
     }
 }
