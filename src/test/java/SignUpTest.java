7fd32cca3d4de78a55c475ce7c353faed7da29a7
Marcin Zieliński <marcin.mariusz.zielinski@gmail.com>
2024-03-14 19:49:21 +0100

    Add sign up invalid email test

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -74,4 +74,39 @@ public class SignUpTest {
         softAssert.assertTrue(errors.contains("The Last Name field is required."));
         softAssert.assertAll();
     }
+
+
+    @Test
+    public void signUpInvalidEmail() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        int randomNumber = (int) (Math.random()*10000);
+
+        WebElement myAccBttn = driver.findElements(By.xpath("//li[@id='li_myaccount']")).get(1);
+        myAccBttn.click();
+        WebElement sngUpBttn = driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1);
+        sngUpBttn.click();
+
+        WebElement firstName = driver.findElement(By.name("firstname"));
+        firstName.sendKeys("Marcin");
+        WebElement lastName = driver.findElement(By.name("lastname"));
+        lastName.sendKeys("Testerski");
+        WebElement phoneNumber = driver.findElement(By.name("phone"));
+        phoneNumber.sendKeys("123456789");
+        WebElement emailAddress = driver.findElement(By.name("email"));
+        emailAddress.sendKeys("test"+randomNumber);
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("test123");
+        WebElement confirmPassword = driver.findElement(By.name("confirmpassword"));
+        confirmPassword.sendKeys("test123");
+        WebElement signUpBttn = driver.findElement(By.xpath("//button[contains(@class, 'signupbtn')]"));
+        signUpBttn.click();
+
+        List<String> errors = driver.findElements(By.xpath("//div[@class='alert alert-danger']//p"))
+                .stream().map(WebElement::getText).toList();
+
+        Assert.assertTrue(errors.contains("The Email field must contain a valid email address."));
+    }
 }
