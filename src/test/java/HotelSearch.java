bf5720da9a0443b85fb474c6653a3e33ba4a511b
Marcin Zieliński <marcin.mariusz.zielinski@gmail.com>
2024-03-14 14:32:51 +0100

    Set check in & check out

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -16,11 +16,26 @@ public class HotelSearch {
         driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
         driver.get("http://www.kurs-selenium.pl/demo/");
 
+        //set city name
         WebElement searchField = driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']"));
         searchField.click();
         WebElement inputField = driver.findElement(By.xpath("//div[@id='select2-drop']//input"));
         inputField.sendKeys("Dubai");
         WebElement textInput = driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']"));
         textInput.click();
+
+        //set check in & check out
+        WebElement checkIn = driver.findElement(By.name("checkin"));
+        checkIn.click();
+        WebElement setDate = driver.findElement(By.name("checkin"));
+        setDate.sendKeys("21/03/2024");
+
+        WebElement checkOut = driver.findElement(By.name("checkout"));
+        checkOut.click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='29']"))
+                .stream()
+                .filter(element -> element.isDisplayed())
+                .findFirst()
+                .ifPresent(element ->  element.click());
     }
 }
